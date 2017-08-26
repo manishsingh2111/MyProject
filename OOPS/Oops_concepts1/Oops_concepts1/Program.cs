026fa13b285b47f6c63fcf533dffc1497b4dae7d
Manish Singh <manis.singh21@gmail.com>
2017-08-26 23:38:55 +0530

    Oops concepts sample project

---
+++ OOPS/Oops_concepts1/Oops_concepts1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_concepts1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MobilePhone samsung = new MobilePhone();
+            samsung.IMEI = "123fghyuio7890";
+            samsung.NoOfSimCards = 2;
+            samsung.Color = "Black";
+
+
+        }
+    }
+
+    class MobilePhone
+    {
+        public string IMEI { get; set; }
+        public int NoOfSimCards { get; set; }
+        public string Color { get; set; }
+
+        public void ConnectCall()
+        {
+
+        }
+    }
+}
